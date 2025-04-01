0d275d90490a2f8769be1feb2c8b9c37f9ce63a6
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-04-01 17:49:12 +0300

    first commit

---
+++ src/test/java/com/example/backend_kutuphane/BackendKutuphaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_kutuphane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendKutuphaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
