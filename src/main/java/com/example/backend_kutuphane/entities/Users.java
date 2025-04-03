65e770b071f5b7a05ee3443407b737705af9090a
Samet Genç <sametgnc496@gmail.com>
2025-04-03 13:03:07 +0300

    User Entity duzenlendi

--- src/main/java/com/example/backend_kutuphane/entities/Users.java
+++ src/main/java/com/example/backend_kutuphane/entities/Users.java
@@ -1,6 +1,7 @@
 package com.example.backend_kutuphane.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,16 +19,20 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String Name;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @Email
     private String email;
+
+    @Column(nullable = false)
     private String password;
-
-
 
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Borrows> borrows;
-
 }
