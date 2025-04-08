0ecfd9dfc29bfd3363d0f9c24029fe705aaa1e5b
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-04-08 13:50:48 +0300

    categories servicde ve contoller eklendi

--- src/main/java/com/example/backend_kutuphane/controllers/CategoriesController.java
+++ src/main/java/com/example/backend_kutuphane/controllers/CategoriesController.java
@@ -1,4 +1,57 @@
 package com.example.backend_kutuphane.controllers;
 
+import com.example.backend_kutuphane.entities.Categories;
+import com.example.backend_kutuphane.services.CategoriesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoriesController {
+    private final CategoriesService categoriesService;
+
+    @Autowired
+    public CategoriesController(CategoriesService categoriesService) {
+        this.categoriesService=categoriesService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categories>>getAllCategories(){
+        List<Categories> categories = categoriesService.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Categories> getCategoryById(@PathVariable("id") Long id) {
+        Optional<Categories> category = categoriesService.getCategoryById(id);
+        return category.map(c -> new ResponseEntity<>(c, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Categories> createCategory(@RequestBody Categories category) {
+        Categories createdCategory = categoriesService.createCategory(category);
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Categories> updateCategory(@PathVariable("id") Long id, @RequestBody Categories categoryDetails) {
+        Categories updatedCategory = categoriesService.updateCategory(id, categoryDetails);
+        if (updatedCategory != null) {
+            return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable("id") Long id) {
+        categoriesService.deleteCategory(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
--- src/main/java/com/example/backend_kutuphane/dto/BooksDTO.java
+++ src/main/java/com/example/backend_kutuphane/dto/BooksDTO.java
@@ -19,5 +19,9 @@ public class BooksDTO {
     private States state;
 
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 }
 
